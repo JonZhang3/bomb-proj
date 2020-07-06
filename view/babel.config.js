de783265922a75e298098098071b3fe4f477239d
Jon <jonzhang3@163.com>
2020-07-06 17:55:47 +0800

    20200706-2

--- view/babel.config.js
+++ view/babel.config.js
@@ -1,12 +1,5 @@
 module.exports = {
     presets: [
-        [
-            "@vue/babel-preset-jsx",
-            {
-                "injectH": false
-            }
-        ],
         '@vue/cli-plugin-babel/preset'
-    ],
-    // plugins: ["transform-vue-jsx"],
+    ]
 }
--- view/src/pages/index/components/project/files/ProjectRecentFiles.vue
+++ view/src/pages/index/components/project/files/ProjectRecentFiles.vue
@@ -1,15 +1,12 @@
 <template>
-    <bomb-table></bomb-table>
+
 </template>
 
 <script>
-
-    import BombTable from "../../../../../components/table/BombTable";
-
     export default {
         name: 'project-recent-files',
         components: {
-            BombTable
+
         },
         data() {
             return {
